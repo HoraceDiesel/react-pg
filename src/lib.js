57135e0bba91671506b1660936743c86e018ef6d
Horace Mac <horace.diesel@gmmail.com>
2017-01-24 21:43:04 +1100

    added css SCSS loaders to webpack and tested

--- src/lib.js
+++ src/lib.js
@@ -1,18 +1,17 @@
 import React from "react";
 import dummys from "./dummys.json";
+import './stylesheets/styles.scss'
 
 export const hello = (
 	<h1 id={dummys[0].id}
-		className="title"
-		style={{background: 'purple', color: 'yellow'}}>
+		className="hello">
 	{ dummys[0].text}
 	</h1>
 )
 
 export const goodbye = (
 	<h1 id="bye"
-		className="title"
-		style={{background: 'yellow', color: 'purple'}}>
+		className="goodbye">
 	{ dummys[1].text }
 	</h1>
 )
