badb13805a00b1e9c3b6ed7ef7a0389b6ef07fc5
Horace Mac <horace.diesel@gmmail.com>
2017-02-04 11:51:23 +1100

    SkiDayList components in index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCounts } from './components/SkiDayCounts.js'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React;
 
 render( 
-	<SkiDayCounts 
-		total={50}
-		powder={20}
-		backcountry={10}
-		goal={200}
+	<SkiDayList days={
+		[
+			{
+				resort: "Squaw Valley",
+				date: new Date("2/1/2017"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Kirkwood",
+				date: new Date("16/1/2017"),
+				powder: false,
+				backcountry: false
+			},
+			{
+				resort: "Mt White",
+				date: new Date("1/2/2017"),
+				powder: false,
+				backcountry: true
+			}
+		]
+	}
 	 />
+	}
 	, document.getElementById('react-container'));
