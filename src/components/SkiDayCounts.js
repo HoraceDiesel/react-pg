7383235cfc72f6a665dc54bcd7bd36b0427fb3db
Horace Mac <horace.diesel@gmmail.com>
2017-02-04 11:47:38 +1100

    Count components completion

--- src/components/SkiDayCounts.js
+++ src/components/SkiDayCounts.js
@@ -1,4 +1,7 @@
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import Snowflake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
 const decimalToPercent = (decimal) => {
 		return ( (decimal * 100 ) + "%");
@@ -11,13 +14,13 @@ const calcGoalProgress = ( total, goal ) => {
 export const SkiDayCounts = ({total, powder, backcountry, goal}) => (
 	<div className="ski-day-count">
 		<div className="total-days">
-			<span>{total} days</span>
+			<span>{total} </span><Calendar /><span>days</span>
 		</div>
 		<div className="powder-days">
-			<span>{powder} days</span>
+			<span>{powder} </span><Snowflake /><span>days</span>
 		</div>
 		<div className="backcountry-days">
-			<span>{backcountry} days</span>
+			<span>{backcountry} </span><Terrain /><span>days</span>
 		</div>
 		<div>
 			<span>
